c88abcbb88063858484a353d22628c0785e33c2c
import-min <minahalaisha@gmail.com>
2026-03-14 14:26:57 -0400

    Validation split

--- nn_basic.py
+++ nn_basic.py
@@ -25,6 +25,11 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+X_train, X_val, y_train, y_val = train_test_split(
+    X_train, y_train, test_size=0.2, random_state=42
+)
+
+
 # Standardize features
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
