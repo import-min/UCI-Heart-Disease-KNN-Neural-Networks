e61eae024b78e9e1dea5a53b95118fbc0a3859d0
import-min <minahalaisha@gmail.com>
2026-04-06 23:25:05 -0400

    Update nn_basic.py

--- nn_basic.py
+++ nn_basic.py
@@ -29,7 +29,6 @@ X_train, X_val, y_train, y_val = train_test_split(
     X_train, y_train, test_size=0.2, random_state=42
 )
 
-
 # Standardize features
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
